1524f9126c577731483b7898ce6aee3c5bd24e62
Kang HyoJun <admire9@gmail.com>
2014-08-28 17:04:51 +0900

    Add foil

---
+++ exam.cfg.py
@@ -0,0 +1,5 @@
+DEBUG = True
+
+DATABASE_URL = "sqlite:///abc.db"
+
+ALEMBIC_SCRIPT_LOCATION = '/Users/admire/src/lyrics/lyrics2feel/migrations/'
--- setup.py
+++ setup.py
@@ -5,8 +5,12 @@ from setuptools import setup, find_packages
 setup(name='lyrics2feel',
       version='0.0.1',
       author='Kang Hyojun',
-      author_email='hyojun@admire.kr',
+      author_email='admire9@gmail.com',
       packages=find_packages(),
       install_requires=[
-          'pytest==2.6.0', 'sphinx'
-      ])
+          'flask==0.10.1', 'flask-script==2.0.5', 'sqlalchemy==0.9.7',
+          'pytest==2.6.0', 'alembic', 'sphinx'
+      ],
+      entry_points={
+          'console_scripts': 'lyrics = lyrics2feel.script:main'
+      })
